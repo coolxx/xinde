05a26ea5d4e75eda76bcfb63a4d37810df722471
henghenghahadada <xing19931122@sina.com>
2017-07-06 16:24:45 +0800

    ceshi

---
+++ js/index.js
@@ -0,0 +1,26 @@
+$(function(){
+	var $height=$('.nav_bg').position().top;
+	var $tt=$('.nav_bg').outerHeight(true)
+	$(document).scroll(function(){
+		var $top=$(document).scrollTop();
+		if($top>=$height){
+			$('.nav_bg').css({'position':'fixed','top':0});
+			$('.subst').css('display','block');
+			$('.cc_l').css({'position':'fixed','top':$tt})
+		}
+	})
+	$(document).scroll(function(){
+		var $top=$(document).scrollTop();
+		if($top<=$height){
+			$('.nav_bg').css('position','');
+			$('.subst').css('display','none');
+			$('.cc_l').css('position','')
+		}
+	})
+	$('.l_area p').each(function(index){
+		$(this).click(function(){
+			$('.l_area p').removeClass('on1').eq(index).addClass('on1');
+			$('.area_sch').hide().eq(index).show()
+		})
+	})
+})
