b1344c7b95a61d1467a022155d179556bf71164c
henghenghahadada <xing19931122@sina.com>
2017-07-06 16:36:33 +0800

    ceshi

--- js/index.js
+++ js/index.js
@@ -20,7 +20,8 @@ $(function(){
 	$('.l_area p').each(function(index){
 		$(this).click(function(){
 			$('.l_area p').removeClass('on1').eq(index).addClass('on1');
-			$('.area_sch').hide().eq(index).show()
+			$('.area_sch').hide().eq(index).show();
+			$('html,body').scrollTop($height)
 		})
 	})
 })
